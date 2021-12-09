287f695ed4d8069d2274e5f821b69599edf1b443
Buffon129 <sebastian3215886999@gmail.com>
2021-12-09 17:23:35 -0500

    barra de generos transicion correcta y efecto hover

--- main.js
+++ main.js
@@ -14,32 +14,32 @@ const hideLeftBarId = document.getElementById('hide-left-bar-id')
 const scrollerId = document.getElementById('scroller-id')
 
 const BagTemporal = nameMangaSearchID.value
+console.log("letras = "+ nameMangaSearchID.value);
 const remplaceEmptyEspaceBetterRead = BagTemporal.replace(/ /g,"%20");
 
 let mangaSearched = "/search/manga?q="+remplaceEmptyEspaceBetterRead+"&page=1"//url to get the object with de manga with similar name
 urlFinal = UrlOfApi+mangaSearched
 
+nameMangaSearchID.addEventListener("keydown", (e)=>{//when you you click de enterkey will search the name of manga in the searchBar
+    if (e.keyCode === 13) {
+        window.location.reload();
+    }
+})
+
 magnifyingGlassIconId.addEventListener("click", (e)=>{//search manga
         window.location.reload();
 })//when you put the name of the manga
 
-btnCategoriesId.addEventListener("click", ()=>{
-    window.location.reload();
+window.addEventListener("KeyboardEvent" ,(e)=>{
+    console.log("la tecla oprimida ="+ e.keyCode);
 })
 
 scrollerId.addEventListener("click", (e)=>{//select genre
-    
     urlFinal2 = UrlOfApi+urlGenre+e.target.value
     let temporalflag = urlFinal2
     urlFinal2 = temporalflag
-    //window.location.reload();
     pageLoaded(urlFinal2)
 })
-
-
-function reloadPageF() {
-    window.location.reload();
-}
 
 function pageLoaded(urlFinal){
     //urlFinal = "https://api.jikan.moe/v3/search/anime?q=&page=1&genre=41"
@@ -54,22 +54,14 @@ fetch(urlFinal)
         idClass = idClass+1; //give a diferent id each manga card 
         const newElementManga = document.createElement('li')
         //const newImageManaga = document.createElement('img')
-        const newNameManga = document.createElement('h2')
+        const newNameManga = document.createElement('h1')
         newNameManga.innerHTML = `${individualManga.title}`
         //newElementManga.insertAdjacentHTML("afterbegin", `<img src="${individualManga.image_url}"></img>`)
         newElementManga.insertAdjacentElement("beforeend", newNameManga)
         newElementManga.setAttribute('id', "image-manga-id"+idClass)
         newElementManga.setAttribute('class', "image-manga")
         newNameManga.setAttribute('class', "name-individual-manga")
-
-        newElementManga.style.position = "relative"
         newElementManga.style.backgroundImage = "url('"+individualManga.image_url+"')"
-        //estilos del texto del nombre del manga
-        newNameManga.style.position = "absolute"
-        newNameManga.style.bottom = "0"
-        newNameManga.style.left = "2%"
-        newNameManga.style.color = "black"
-        newNameManga.style.textShadow = "-1.5px 0 white, 0 1.5px white, 1.5px 0 white, 0 -1.5px white"
         newElementManga.appendChild(newNameManga)
         fragmenteWithMangas.appendChild(newElementManga)
         urlFinal = ''
@@ -87,13 +79,13 @@ fetch(urlFinal)
 })
 .catch(err => console.log("hay un problema "+err))
 }
-
 pageLoaded(urlFinal)
 
-//agregar la barra con los generos de manga, y al dar click te muestre ejemplos
-//al dar click sobre la tarjeta te de info extra
-//colocar algun tipo de contenido po defecto al abrir el sitio, podria hacerse usando un if
-//el logo me redirija a la documentacion de la api
+//---show genre bar when user click on button---
+btnCategoriesId.addEventListener("click", (e)=>{
+    e.preventDefault();
+    hideLeftBarId.classList.toggle("show-left-bar")
+})
+
+//colocar algun tipo de contenido por defecto al abrir el sitio, podria hacerse usando un if
 //podria hacer que al hacer hover muestre info extra
-//manga genre reset de page
-//en caso de a cual hagan click
